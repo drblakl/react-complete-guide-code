69b875934c62b0a1a88da00f5d1699dd70ae7948
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-23 08:18:47 -0400

    Checking in before creating first component

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -1,9 +1,12 @@
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    // JSX Code (Javascript XML)
+    // Developer Friendly code transformed behind the scenes
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <p>This is also visible!</p>
+        </div>
+    );
 }
 
 export default App;
