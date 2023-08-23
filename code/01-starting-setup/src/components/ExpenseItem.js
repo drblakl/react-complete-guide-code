5baaaaf6fd61c42ef1e4c3fc8175ae81b82476a0
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-23 12:41:13 -0400

    3.51 - Splitting Components

--- code/01-starting-setup/src/components/ExpenseItem.js
+++ code/01-starting-setup/src/components/ExpenseItem.js
@@ -1,20 +1,14 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 // A Component in React is a javascript function
 // Can also use object destructuring in param list
 //function ExpenseItem(date, title, amount) {
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
