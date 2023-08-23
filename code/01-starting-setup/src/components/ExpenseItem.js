5d47e69b1acea00b3b3a38dcd46248c7a0b2c699
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-23 08:31:46 -0400

    Section 3.45

--- code/01-starting-setup/src/components/ExpenseItem.js
+++ code/01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
 // A Component in React is a javascript function
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div>
+            <div>March 28th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$249.67</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
