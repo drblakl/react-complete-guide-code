4252e27758e8d48a333584478ed2eaf5634b8217
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-24 21:36:00 -0400

    77 - Updating State

--- code/01-working-with-state/src/components/NewExpense/ExpenseForm.js
+++ code/01-working-with-state/src/components/NewExpense/ExpenseForm.js
@@ -8,16 +8,42 @@ const ExpenseForm = () => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    //const [userInput, setUserInput] = useState({
+    //    enteredTitle: '',
+    //    enteredAmount: '',
+    //    enteredDate: ''
+    //});
+
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    enteredTitle: event.target.value
+        //});
+        // Ensures we get the latest state snapshot
+        // If state update relies on previous state
+        //setUserInput((prevState) => {
+        //    return {
+        //        ...prevState,
+        //        enteredTitle: event.target.value
+        //    };
+        //});
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    enteredAmount: event.target.value
+        //});
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    enteredDate: event.target.value
+        //});
     };
 
   return (
