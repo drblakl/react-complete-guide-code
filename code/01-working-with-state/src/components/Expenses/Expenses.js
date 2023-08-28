fd83e114b8719aad3269b124a83665b6e80d2dab
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-28 19:01:55 -0400

    82 - Derived / Computed State

--- code/01-working-with-state/src/components/Expenses/Expenses.js
+++ code/01-working-with-state/src/components/Expenses/Expenses.js
@@ -9,9 +9,22 @@ const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState("2020");
 
+    let filterInfoText = '2019, 2021 & 2022';
+
+    if (filteredYear === '2019') {
+        filterInfoText = "2020, 2021 & 2022";
+    }
+    else if (filteredYear === '2021') {
+        filterInfoText = "2019, 2020 & 2022";
+    }
+    else if (filteredYear === '2022') {
+        filterInfoText = "2019, 2020 & 2021";
+    }
+
     const addFilterHandler = (year) => {
         setFilteredYear(year);
         console.log("YEAR : " + year);
+
     };
 
 
@@ -19,6 +32,7 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter onFilterChange={addFilterHandler} selected={filteredYear} />
+                <p>{filterInfoText}</p>
                 <ExpenseItem
                     title={props.items[0].title}
                     amount={props.items[0].amount}
