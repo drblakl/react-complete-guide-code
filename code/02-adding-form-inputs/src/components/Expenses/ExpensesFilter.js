859014475e2b2c09af692e6b3efa7a49f7e36322
BlakleyD <devin.blakley@claytonhomes.com>
2023-08-28 18:53:08 -0400

    Exercise to put it all together.

---
+++ code/02-adding-form-inputs/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+
+    const [selectedYear, setSelectedYear] = useState('');
+
+    const yearChangeHandler = (event) => {
+        setSelectedYear(event.target.value);
+        console.log("Filter User selected: " + selectedYear);
+        props.onFilterChanged(selectedYear);
+    }
+
+
+    return (
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter by year</label>
+                <select onChange={yearChangeHandler}>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
