cc13fd4f2cac1f054088e5b549a5098751bd3e33
BlakleyD <devin.blakley@claytonhomes.com>
2023-09-25 17:42:01 -0400

    End of module

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,9 +1,22 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 import Counter from './components/Counter';
-
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return (
-    <Counter />
+
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+    return (
+        <Fragment>
+            {isAuthenticated && <Header />}
+            {!isAuthenticated && <Auth />}
+            {isAuthenticated && <UserProfile />}
+            {isAuthenticated && <Counter />}
+            
+        </Fragment>    
   );
 }
 
