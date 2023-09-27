7431aa4d7baba2cc570d96e5fe4adb111a51cc53
BlakleyD <devin.blakley@claytonhomes.com>
2023-09-26 23:44:40 -0400

    end of day check-in

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,5 +1,33 @@
+import {
+    createBrowserRouter, RouterProvider,
+    //createRoutesFromElements, Route
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+//const routeDefinitions = createRoutesFromElements(
+//    <Route>
+//        <Route path="/" element={<HomePage />}></Route>
+//        <Route path="/products" element={<ProductsPage />}></Route>
+//    </Route>
+//);
+
+//const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: '/', element: <HomePage /> },
+            { path: '/products', element: <ProductsPage /> }
+        ]
+    },
+]);
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router } />;
 }
 
 export default App;
